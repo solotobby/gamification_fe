5a5f4a4b15aae1e84ccb1662a0bb4ad2ba73d45c
Whoistolu <digittolu25@gmsil.com>
2024-04-02 12:08:26 +0100

    Fixed a style for the ourgoal component

--- src/components/landing-page/OurGoal.jsx
+++ src/components/landing-page/OurGoal.jsx
@@ -1,9 +1,10 @@
 const OurGoal = () => {
     return (
-            <div className=" bg-customGray">
+           <div className="my-8">
+             <div className="my-8 bg-customGray">
                 <div className="container flex">
                     <div className="flex flex-col justify-center ite-center">
-                        <h1>Our goal</h1>
+                        <h1 className="text-2xl font-bold ">Our goal</h1>
                         <p className="w-2/5">
                             Asides putting a smile on many faces by providing a mean to earn online, we aim at supporting hardworking creators,
                             business owners, and organizations to gain meaningful organic engagement that will translate to massive sales.
@@ -12,11 +13,12 @@ const OurGoal = () => {
                             The goal is a build a community that is ready to give value and earn from it.
                         </p>
                     </div >
-                    <div className="w-1/3 p-4">
+                    <div className="w-4/5 py-20 my-8">
                          <img src="/images/Clapping.png" alt="ourgoal-image" className="w-full h-auto" />
                     </div>
                 </div>
             </div>
+           </div>
     )
 }
 
