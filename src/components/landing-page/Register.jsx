bbf1946725c50296b1d2e6ad43947a72d30a1b40
Whoistolu <digittolu25@gmsil.com>
2024-05-21 19:14:21 +0100

    push more functions

--- src/components/landing-page/Register.jsx
+++ src/components/landing-page/Register.jsx
@@ -3,19 +3,19 @@ import { Link, useNavigate } from 'react-router-dom';
 
 const Register = () => {
     const [countries, setCountries] = useState([]);
-  
+
     const [selectedDialingCode, setSelectedDialingCode] = useState('+234');
-   
+
     const [values, setValues] = useState({
-        "email" : "",
-        "first_name" : "",
-        "last_name" : "",
-        "country" : "Nigeria",
-        "phone" : "",
-        "password" : "",
-        "password_confirmation" : "",
-        "source" : "Facebook",
-        "ref_id" : ""
+        "email": "",
+        "first_name": "",
+        "last_name": "",
+        "country": "Nigeria",
+        "phone": "",
+        "password": "",
+        "password_confirmation": "",
+        "source": "Facebook",
+        "ref_id": ""
     });
     const [showPassword, setShowPassword] = useState(false);
     const navigate = useNavigate();
@@ -28,8 +28,8 @@ const Register = () => {
     });
 
     const handleInputValues = (e) => {
-        const {value, name} = e.target
-        setValues((prev)=> {
+        const { value, name } = e.target
+        setValues((prev) => {
             return {
                 ...prev,
                 [name]: value
@@ -39,8 +39,8 @@ const Register = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        
-        const formObject = {...values};
+
+        const formObject = { ...values };
         try {
             const response = await fetch('http://app.e-portal.com.ng/api/register', {
                 method: 'POST',
@@ -49,21 +49,21 @@ const Register = () => {
                 },
                 body: JSON.stringify(formObject),
             });
-    
+
             const data = await response.json();
-    
+
             if (response.status === 201) {
-               
+
                 console.log('Registration successful:', data);
                 navigate('/login');
             } else {
-                
+
                 console.error('Failed to register:', data);
-                
+
             }
         } catch (error) {
             console.error('Error registering:', error);
-            
+
         }
     };
 
@@ -78,7 +78,7 @@ const Register = () => {
                 throw new Error('Failed to fetch country list');
             }
             const data = await response.json();
-            
+
             setCountries(data);
         } catch (error) {
             console.error('Error fetching country list:', error);
@@ -87,16 +87,16 @@ const Register = () => {
 
 
     const handleCountryChange = (e) => {
-       
-    
+
+
         const selectedCountryObject = countries.find(country => country.country === values.country);
-    
+
         if (selectedCountryObject) {
             setSelectedDialingCode(selectedCountryObject.dialing_code);
         }
     };
 
-   
+
 
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword);
@@ -155,12 +155,12 @@ const Register = () => {
                             </div>
                             <div className="flex flex-col form-group ">
                                 <label htmlFor="last-name">Last name</label>
-                                <input type="text" id="Last-name" name="last_name" className="w-full rounded-md" value={values.last_name} onChange={handleInputValues}  />
+                                <input type="text" id="Last-name" name="last_name" className="w-full rounded-md" value={values.last_name} onChange={handleInputValues} />
                             </div>
                         </div>
                         <div className="flex flex-col mt-4 form-group">
                             <label htmlFor="email-address">Email address</label>
-                            <input type="text" id="email-address" name="email" className="rounded-md" value={values.email} onChange={handleInputValues}  />
+                            <input type="text" id="email-address" name="email" className="rounded-md" value={values.email} onChange={handleInputValues} />
                         </div>
                         <div className="flex flex-col mt-4 form-group">
                             <label htmlFor="country">Country</label>
@@ -177,7 +177,7 @@ const Register = () => {
                             <select id="phone-code" name="phone" className="w-24 rounded-md" value={selectedDialingCode} readOnly>
                                 <option value={selectedDialingCode}>{selectedDialingCode}</option>
                             </select>
-                            <input type="text" id="phone-number" name="phone" className="w-full rounded-md" placeholder="Enter your phone number" value={values.phone} onChange={handleInputValues}  />
+                            <input type="text" id="phone-number" name="phone" className="w-full rounded-md" placeholder="Enter your phone number" value={values.phone} onChange={handleInputValues} />
                         </div>
                         <div className="relative flex flex-col mt-4 form-group">
                             <label htmlFor="password">Password</label>
@@ -202,31 +202,31 @@ const Register = () => {
                                 </div>
                                 <div>
                                     <input type="radio" id="uppercase" checked={passwordStrength.uppercase} readOnly style={{ color: getRadioColor(passwordStrength.uppercase) }} />
-                                    <label htmlFor="uppercase" style={{ color: getRadioColor(passwordStrength.uppercase), marginLeft: '8px'  }}>Upper case character</label>
+                                    <label htmlFor="uppercase" style={{ color: getRadioColor(passwordStrength.uppercase), marginLeft: '8px' }}>Upper case character</label>
                                 </div>
                                 <div>
                                     <input type="radio" id="lowercase" checked={passwordStrength.lowercase} readOnly style={{ color: getRadioColor(passwordStrength.lowercase) }} />
-                                    <label htmlFor="lowercase" style={{ color: getRadioColor(passwordStrength.lowercase), marginLeft: '8px'  }}>Lower case character</label>
+                                    <label htmlFor="lowercase" style={{ color: getRadioColor(passwordStrength.lowercase), marginLeft: '8px' }}>Lower case character</label>
                                 </div>
                                 <div>
                                     <input type="radio" id="special-character" checked={passwordStrength.specialCharacter} readOnly style={{ color: getRadioColor(passwordStrength.specialCharacter) }} />
-                                    <label htmlFor="special-character" style={{ color: getRadioColor(passwordStrength.specialCharacter), marginLeft: '8px'  }}>One special character</label>
+                                    <label htmlFor="special-character" style={{ color: getRadioColor(passwordStrength.specialCharacter), marginLeft: '8px' }}>One special character</label>
                                 </div>
                                 <div>
                                     <input type="radio" id="number" checked={passwordStrength.number} readOnly style={{ color: getRadioColor(passwordStrength.number) }} />
-                                    <label htmlFor="number" style={{ color: getRadioColor(passwordStrength.number), marginLeft: '8px'  }}>One number</label>
+                                    <label htmlFor="number" style={{ color: getRadioColor(passwordStrength.number), marginLeft: '8px' }}>One number</label>
                                 </div>
                             </div>
                         </div>
                         <div className="flex flex-col mt-4 form-group">
                             <label htmlFor="confirm-password">Confirm Password</label>
-                            <input type="password" id="confirm-password" name="password_confirmation"  className="rounded-md" value={values.password_confirmation} onChange={handleInputValues} />
+                            <input type="password" id="confirm-password" name="password_confirmation" className="rounded-md" value={values.password_confirmation} onChange={handleInputValues} />
                         </div>
                         <div className="flex flex-col mt-4 form-group">
                             <label htmlFor="referral-code">Referral Code(Optional)</label>
                             <input type="text" id="referral-code" name="ref_id" className="rounded-md" value={values.ref_id} onChange={handleInputValues} />
                         </div>
-                        
+
                         <div className="flex flex-col mt-4 form-group">
                             <label htmlFor="how-heard">How did you hear about Freebyz?</label>
                             <select id="how-heard" name="source" className="rounded-md">
@@ -241,8 +241,15 @@ const Register = () => {
                                 <option value="FriendReferral">Referred by a friend</option>
                             </select>
                         </div>
+                        <div className="flex items-center mt-4">
+                            <input type="checkbox" id="agree-checkbox" name="agree-checkbox" />
+                            <label htmlFor="agree-checkbox" className="ml-2">
+                                I have read, understood, and agreed to Freebyz
+                                <Link to="/privacy-policy" className="text-blue-500"> Privacy policy </Link>
+                                and <Link to="/terms-of-use" className="text-blue-500"> Terms of use. </Link>
+                            </label>
+                        </div>
 
-                        
                         <div className="flex my-8">
                             <button className="px-4 py-2 text-white bg-blue-500 rounded-full" type="submit">Sign up</button>
                             <p className="mx-8">or</p>
@@ -253,14 +260,7 @@ const Register = () => {
                         </div>
                     </div>
                 </form>
-                <div className="flex items-center mt-4">
-                            <input type="checkbox" id="agree-checkbox" name="agree-checkbox" />
-                            <label htmlFor="agree-checkbox" className="ml-2">
-                                I have read, understood, and agreed to Freebyz
-                                <Link to="/privacy-policy" className="text-blue-500"> Privacy policy </Link>
-                                and terms of use.
-                            </label>
-                        </div>
+                
             </div>
         </div>
     );
