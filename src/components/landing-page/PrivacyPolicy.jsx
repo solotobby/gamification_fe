afa7930c186392f39e80d45a0da3963e10c400ce
Whoistolu <digittolu25@gmsil.com>
2024-05-21 19:46:47 +0100

    renamed the useRef

--- src/components/landing-page/PrivacyPolicy.jsx
+++ src/components/landing-page/PrivacyPolicy.jsx
@@ -4,7 +4,7 @@ const PrivacyPolicy = () => {
     const overviewRef = useRef()
     const childInfoRef = useRef()
     const infoWeCollectRef = useRef()
-    const cconsent = useRef()
+    const consent = useRef()
     const hoWeManageYourInfoRef = useRef()
     const logFilesRef = useRef()
     const cookiesAndWebRef = useRef()
@@ -100,16 +100,16 @@ const PrivacyPolicy = () => {
                             These are not linked to any information that is personally identifiable.
                         </p>
                         <p className="mb-4">
-                            The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
+                            The purpose of the information is for analyzing trends, administering the site, tracking users&apos; movement on the website, and gathering demographic information.
                         </p>
                     </div>
                     <p className="mb-4 text-2xl font-bold">Cookies and web beacons</p>
                     <div ref={cookiesAndWebRef}>
                         <p className="mb-4">
-                            Like any other website, Freebyz uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited.
+                            Like any other website, Freebyz uses &apos;cookies&apos;. These cookies are used to store information including visitors&apos; preferences, and the pages on the website that the visitor accessed or visited.
                         </p>
                         <p className="mb-4">
-                            The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.
+                            The information is used to optimize the users' experience by customizing our web page content based on visitors&apos; browser type and/or other information.
                         </p>
                     </div>
                     <p className="mb-4 text-2xl font-bold">
@@ -118,7 +118,7 @@ const PrivacyPolicy = () => {
                     <div ref={advertisingPartnersRef}>
                         <p className="mb-4">
                             You may consult this list to find the Privacy Policy for each of the advertising partners of Freebyz. Third-party ad servers or ad networks uses technologies like cookies, JavaScript,
-                            or Web Beacons that are used in their respective advertisements and links that appear on Freebyz, which are sent directly to users' browser.
+                            or Web Beacons that are used in their respective advertisements and links that appear on Freebyz, which are sent directly to users&apos; browser.
                         </p>
                         <p className="mb-4">
                             They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
@@ -134,12 +134,12 @@ const PrivacyPolicy = () => {
                             It may include their practices and instructions about how to opt-out of certain options. You can choose to disable cookies through your individual browser options.
                         </p>
                         <p className="mb-4">
-                            To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites. CCPA Privacy Rights (Do Not Sell My Personal Information). Under the CCPA, among other rights, California consumers have the right to:
+                            To know more detailed information about cookie management with specific web browsers, it can be found at the browsers&apos; respective websites. CCPA Privacy Rights (Do Not Sell My Personal Information). Under the CCPA, among other rights, California consumers have the right to:
                         </p>
                         <ul className="pl-6 list-decimal">
-                            <li className="mb-4">Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</li>
+                            <li className="mb-4">Request that a business that collects a consumer&apos;s personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</li>
                             <li className="mb-4">Request that a business delete any personal data about the consumer that a business has collected.</li>
-                            <li className="mb-4">Request that a business that sells a consumer's personal data, not sell the consumer's personal data..</li>
+                            <li className="mb-4">Request that a business that sells a consumer&apos;s personal data, not sell the consumer&apos;s personal data..</li>
                         </ul>
                         <p className="mb-4">If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
                     </div>
@@ -160,7 +160,7 @@ const PrivacyPolicy = () => {
                    </div>
 
                     <div ref={childInfoRef}>
-                        <p className="mb-4 text-2xl font-bold">Children's information</p>
+                        <p className="mb-4 text-2xl font-bold">Children&apos;s information</p>
                         <p className="mb-4">
                             Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
                         </p>
