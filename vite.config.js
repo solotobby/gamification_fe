31dc8845e194a1b151b362437ef8f3db54f554c0
Whoistolu <digittolu25@gmsil.com>
2024-06-12 10:18:12 +0100

    modify the vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  preview: {
+    port: 3001,
+  },
+
+  server: {
+    port: 3006,
+  },
 })
