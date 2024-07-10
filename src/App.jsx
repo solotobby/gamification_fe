f8bb10a54b1cd55e969810474ee345ba30978326
Ajise Toluwase <digittolu25@gmail.com>
2024-07-10 17:50:08 +0100

    Revert "Landing page"

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from "./components/landing-page/Header";
-import Home from './components/landing-page/pages/Home.jsx';
-import ContactUs from "./components/landing-page/ContactUs.jsx";
-import Footer from './components/landing-page/Footer.jsx';
-import AboutUs from './components/landing-page/AboutUs.jsx';
-import TermsOfUse from './components/landing-page/TermsOfUse.jsx';
-import PrivacyPolicy from './components/landing-page/PrivacyPolicy.jsx';
-import ForgotPassword from './components/landing-page/ForgotPassword.jsx';
-import Login from './components/landing-page/Login.jsx';
-import Register from './components/landing-page/Register.jsx';
-import Otp from './components/landing-page/Otp.jsx';
-import ResetPassword from './components/landing-page/ResetPassword.jsx';
-import Dashboard from './components/landing-page/Dashboard.jsx';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Router>
-      <Routes>
-       
-        <Route path="/" element={<><Header /><Home /><Footer /></>} />
-        <Route path="/contact-us" element={<><Header /><ContactUs /><Footer /></>} />
-        <Route path="/about-us" element={<><Header /><AboutUs /><Footer /></>} />
-        <Route path="/terms-of-use" element={<><Header /><TermsOfUse /><Footer /></>} />
-        <Route path="/privacy-policy" element={<><Header /><PrivacyPolicy /><Footer /></>} />
-        <Route path="/dashboard" element={<><Header /><Dashboard /><Footer /></>} />
-        
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/otp" element={<Otp />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
