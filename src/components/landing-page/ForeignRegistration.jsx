d5044e4143957c9f6da5f9da7b09620122439547
Whoistolu <digittolu25@gmsil.com>
2024-09-14 19:20:49 +0100

    Re-adjust the style for the sign-up

--- src/components/landing-page/ForeignRegistration.jsx
+++ src/components/landing-page/ForeignRegistration.jsx
@@ -6,16 +6,15 @@ const ForeignRegistration = () => {
     const [email, setEmail] = useState("");
     
     return (
-        <div className="relative grid h-screen grid-cols-1 bg-white md:grid-cols-3">
-           
-            <div className="w-full h-32 pl-2 md:pl-8 md:h-full bg-customBlue left-class shrink-0 background-map2">
-                <img className="pt-10" src="/images/Freebyz-logo-white.png" alt="freebyz-logo" />
-                <h1 className="w-1/2 mt-20 text-2xl text-white">Work online and earn daily in dollar and naira!</h1>
-                
-            </div>
-    
-            
-            <div className='flex h-full col-span-2'>
+        <div className="relative h-screen overflow-scroll">
+            <div className="relative grid h-auto grid-cols-1 gap-32 md:grid-cols-3 md:h-full md:gap-0">
+                <div className="w-full h-32 pl-2 md:pl-8 md:h-full bg-customBlue left-class shrink-0 background-map2">
+                    <img className="pt-10" src="/images/Freebyz-logo-white.png" alt="freebyz-logo" />
+                    <h1 className="hidden w-1/2 mt-20 text-2xl text-white md:block">Work online and earn daily in dollar and naira!</h1>
+                    
+                </div>
+
+                <div className='flex h-auto col-span-1 md:col-span-2'>
                 <div className="flex flex-col mt-0 md:mt-60 login-content lg:w-[40%] ml-0 md:ml-10 lg:ml-32 w-full px-4 md:px-0">
                     <h2 className="text-3xl font-bold">Sign up for free</h2>
                     <p className="text-gray-400">Please ensure that you provide your active email address</p>
@@ -44,11 +43,17 @@ const ForeignRegistration = () => {
                 </div>
             </div>
             
-            <div className='absolute top-32 right-8 md:top-10'>
-                <p className="mx-8 mt-10">
+            <div className='absolute right-100 top-40 md:right-8 md:top-10'>
+                <p className="mx-5">
                     Already have an account? <Link to="/login" className="text-blue-500">Login</Link>
                 </p>
             </div>
+            </div>
+           
+           
+    
+            
+           
         </div>
     );
    
