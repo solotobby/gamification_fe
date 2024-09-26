15d864a64c15c32fb1625be187d98e714fed912a
Whoistolu <digittolu25@gmail.com>
2024-09-26 18:49:47 +0100

    Remove some of the routes and checked out to another branch

--- src/App.jsx
+++ src/App.jsx
@@ -12,30 +12,9 @@ import Register from './components/landing-page/Register.jsx';
 import Otp from './components/landing-page/Otp.jsx';
 import ResetPassword from './components/landing-page/ResetPassword.jsx';
 import Dashboard from './components/landing-page/Dashboard.jsx';
-import Sidenav from './components/dashboard/SideNav.jsx';
-import DashboarDollar from './components/dashboard/DashboarDollar.jsx';
-import ResetLinkSent from './components/landing-page/ResetLinkSent.jsx';
-import MakeMoney from './components/landing-page/MakeMoney.jsx';
-import  CreateCampaign from './components/dashboard/campaign/CreateCampaign.jsx'
-import ViewCampaign from './components/dashboard/campaign/ViewCampaign.jsx';
-import ApprovedCampaign from './components/dashboard/campaign/ApprovedCampaign.jsx';
-import DeniedCampaign from './components/dashboard/campaign/DeniedCampaign.jsx';
-import YourWallet from './components/dashboard/wallet/YourWallet.jsx';
-import WithdrawalRequests from './components/dashboard/wallet/WithdrawalRequests.jsx';
-import DashboardNaira from './components/dashboard/DashboadNaira.jsx';
-import FirstJob from './components/dashboard/FirstJob.jsx';
-import AvailableJobs from './components/dashboard/jobs/AvailableJobs.jsx';
-import MyJobs from './components/dashboard/jobs/MyJobs.jsx';
-import CompletedJobs from './components/dashboard/jobs/CompletedJobs.jsx';
-import DisputedJobs from './components/dashboard/jobs/DisputedJobs.jsx';
-import CreateBannerAds from './components/dashboard/bannerAds/CreateBannerAds.jsx';
-import ListBannerAds from './components/dashboard/bannerAds/ListBannerAds.jsx';
-import Referral from './components/dashboard/referral/Referral.jsx';
-import TalkToUs from './components/dashboard/talk to us/TalkToUs.jsx';
-import OpenNewticket from './components/dashboard/talk to us/OpenNewTicket.jsx';
-import CurrncyConverter from './components/dashboard/currency converter/CurrencyConverter.jsx';
 import NigeriaRegistration from './components/landing-page/NigeriaRegistration.jsx';
 import ForeignRegistration from './components/landing-page/ForeignRegistration.jsx';
+import MakeMoney from './components/landing-page/MakeMoney.jsx';
 
 
 function App() {
@@ -50,37 +29,14 @@ function App() {
         <Route path="/privacy-policy" element={<><Header /><PrivacyPolicy /><Footer /></>} />
         <Route path="/dashboard" element={<><Header /><Dashboard /><Footer /></>} />
         <Route path="/" element={<><Header /><Dashboard /><Footer /></>} />
-        <Route path="/make-money" element={<><Header /><MakeMoney /><Footer /></>} />
-
-        
         <Route path="/registration-form" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/otp" element={<Otp />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/sidenav" element={<Sidenav />} />
-        <Route path="/dashboard-dollar" element={<DashboarDollar />} />
-        <Route path="/dashboard-naira" element={<DashboardNaira />} />
-        <Route path="/reset-link" element={<ResetLinkSent />} />
-        <Route path="/dashboard-dollar/create-campaign" element={<CreateCampaign />} />
-        <Route path="/dashboard-dollar/view-campaign" element={<ViewCampaign />} />
-        <Route path="/dashboard-dollar/approved-campaign" element={< ApprovedCampaign />} />
-        <Route path="/dashboard-dollar/denied-campaign" element={< DeniedCampaign />} />
-        <Route path="/dashboard-dollar/your-wallet" element={< YourWallet />} />
-        <Route path="/dashboard-dollar/withdrawal-request" element={<  WithdrawalRequests />} />
-        <Route path="/first-job" element={<  FirstJob />} />
-        <Route path="/available-jobs" element={< AvailableJobs />} />
-        <Route path="/my-jobs" element={<  MyJobs />} />
-        <Route path="/completed-jobs" element={<  CompletedJobs />} />
-        <Route path="/disputed-jobs" element={<  DisputedJobs />} />
-        <Route path="/create-banner-ads" element={<   CreateBannerAds />} />
-        <Route path="/list-banner-ads" element={<  ListBannerAds />} />
-        <Route path="/referral" element={< Referral />} />
-        <Route path="/talk-to-us" element={< TalkToUs />} />
-        <Route path="/open-new-ticket" element={< OpenNewticket />} />
-        <Route path="/currency-converter" element={< CurrncyConverter />} />
         <Route path="/registration-nigeria" element={< NigeriaRegistration />} />
         <Route path="/registration" element={< ForeignRegistration />} />
+        <Route path="/make-money" element={<><Header /><MakeMoney /><Footer /></>} />
         
       
       </Routes>
