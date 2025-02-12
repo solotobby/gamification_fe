813eacbfeeed094f7565bb61ffa5d640781da1c6
Whoistolu <digittolu25@gmail.com>
2025-02-12 16:18:44 +0100

    Refacor TermOfUse component

--- src/components/landing-page/TermsOfUse.jsx
+++ src/components/landing-page/TermsOfUse.jsx
@@ -1,42 +1,38 @@
 const TermsOfUse = () => {
+    const terms = [
+        "By signing up to Freebyz, you agree to create a wallet where you can access remote/micro tasks as an online worker or a business owner/creator/entrepreneur who wants to post online social media and special digital jobs for online workers.",
+        "Your account must be used solely for that purpose or purposes as updated on the website.",
+        "Your account may be suspended if you attempt to spam people or post spurious jobs.",
+        "All Campaigns/Jobs will be screened before approval.",
+        "Every Job (Campaign) you advertise must be approved within 24hrs otherwise those campaigns (Jobs) will be automatically approved.",
+        "All users must be verified to access highly paid jobs or withdraw from wallet/Post Jobs.",
+        "All users can use referral codes to refer friends.",
+        "A sum of 3000 NGN (or equivalent in your currency) is required to verify an account. Withdrawal can be requested any time but payment is only made on Fridays. Minimum withdrawal is 2,500 NGN with 5% withdrawal charges. Withdrawal requests made before 12pm on Fridays will be processed next Friday. Withdrawal will only be made to your bank account or PayPal that matches exactly the names used in registration. Disparities in names can delay payment(s).",
+        "You are paid a sum of N1000 (or equivalent in your currency) on every friend you refer to Freebyz using your affiliate link (available on your dashboard).",
+        "Unverified users can refer friends but cannot post jobs or make withdrawal(s).",
+        "You can play games and quizzes to earn points which can be converted to money."
+    ];
+
     return (
         <div className="container pt-10 md:pt-20">
-            <h1 className="my-8 text-3xl font-bold">Terms of use</h1>
-            <div className="text-base leading-relaxed">
-                <p className="mb-4 font-inter">
-                1. By signing up to Freebyz, you agree to create a wallet where you can either make money as an affiliate/social media worker
-                or a business owner/creator/entrepreneur who wants to post online social media and special digital jobs for online workers.
-                <p className="mb-4 font-inter"> 2. You account must be used solely for that purpose or purposes as updated on the website.</p>
-                <p className="mb-4 font-inter">3. Your accounat may be suspended if you attempt to spam people or post spurious jobs.</p>
-                <p className="mb-4 font-inter">4. All Jobs will be screened before approval</p>
-                <p>5. Every Job you advertise must be approved within 5days otherwise those Jobs will be automatically approved.</p>
-                <p className="mb-4 font-inter">
-                    6. All users must be verified to:
-                    <ul className="ml-6 list-disc font-inter">
-                        <li>access more than 2 jobs</li>
-                        <li>withdraw from wallet</li>
-                        <li>Post Jobs</li>
-                    </ul>
+            <h1 className="my-8 text-3xl font-bold">Terms of Use</h1>
+            <div className="text-base leading-relaxed font-graphik">
+                <ol className="space-y-4 list-decimal list-inside font-graphik">
+                    {terms.map((term, index) => (
+                        <li key={index}>
+                            {term}
+                        </li>
+                    ))}
+                </ol>
+                <p className="mt-8">
+                    This Terms of Use will be updated regularly. You will find the updates in your dashboard.
                 </p>
-                <p className="mb-4 font-inter"> 7. All users can use referral codes to refer friends.</p>
-                <p>
-                    8. A sum of 1050 NGN is required to verify an account. Withdrawal can be requested any time but payment is only made on Fridays.
-                        Minimum withdrawal is 2,500 NGN with 5% withdrawal charges. Withdrawal requests made before 12pm on Fridays will be processed next Friday.
-                        Withdrawal will only be made to your bank account or PayPal that matches exactly the names used in registration. Disparities in names can delay payment (s).
-                </p>
-                <p className="mb-4 font-inter"> 9. You are paid a sum of N500 on every friend you refer to Freebyz using your affiliate link (available on your dashboard).</p>
-                <p className="mb-4 font-inter"> 10. Unverified users can refer friends but cannot post jobs or make withdrawal (s).</p>
-                <p className="mb-4 font-inter"> 11. You can play games and quizzes to earn points which can be converted to money.</p>
-                    </p>
-                    <p className="mb-8 font-inter">
-                    This Terms of Use will be updated regularly.You will find the updates in your dashboard.
-                    </p>
-                    <p className="mb-4 font-inter">
+                <p className="mt-4">
                     Thank you for using Freebyz.
-                    </p>
+                </p>
             </div>
         </div>
-    )
+    );
 }
 
 export default TermsOfUse;
