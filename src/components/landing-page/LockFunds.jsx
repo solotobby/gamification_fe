a5bb364ffbe625fa72055833c8afca2c04d91f78
Whoistolu <digittolu25@gmail.com>
2025-02-12 16:17:23 +0100

    Update LockFunds component

--- src/components/landing-page/LockFunds.jsx
+++ src/components/landing-page/LockFunds.jsx
@@ -4,7 +4,8 @@ const LockFunds = () => {
             <img src="/images/arrow-right.png" alt="right-arrow" className="w-8 h-auto mr-4 md:w-12 md:mr-4" />
             <div className="max-w-xs p-4 md:max-w-xl bg-customGray">
                 <p className="text-base md:text-base">
-                    Lock funds to earn up to 5% interest in 3-6 months. Your funds are 100% safe and secure. Your interest would be paid to your local bank account at maturity.
+                Freebyz is available Globally.
+                Hire Real time online workers you can track. Our online workers are ready to submit evidence for every micro task performed.
                 </p>
             </div>
         </div>
