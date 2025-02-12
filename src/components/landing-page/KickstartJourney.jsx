8e774d1f9a697dc0dd34bd5dd234557505092f09
Whoistolu <digittolu25@gmail.com>
2025-02-12 16:16:56 +0100

    Update kickstart journey component

--- src/components/landing-page/KickstartJourney.jsx
+++ src/components/landing-page/KickstartJourney.jsx
@@ -5,27 +5,28 @@ import { Link } from "react-router-dom";
 
 const KickstartJourney = () => {
     return (
-        <div className="container mt-8">
-            
-            <p className="w-full mt-20 text-2xl font-medium leading-10 font-bricolage-grotesque md:text-5xl md:w-3/4">
-                Kick-start your journey to financial emancipation
-            </p>
-            <p className="mb-8">Here’s how Freebyz works.</p>
+        <div className="bg-[#F0F8FF] py-2">
+            <div className="container mt-8">
 
-            
-            <div className="hidden gap-4 md:flex">
-                {kickstartJourney.map((el, i) => (
-                    <div key={i} className="flex-grow p-5 bg-customGray">
-                        <p className='text-3xl font-medium leading-10 md:text-base'>{el.title}</p>
-                        <p className="md:text-base">{el.description}</p>
-                        <img src={el.image} alt="kickstart-journey-image" />
-                    </div>
-                ))}
-            </div>
+                <p className="w-full text-2xl font-medium leading-10 md:mt-20 font-bricolage-grotesque md:text-5xl md:w-3/4">
+                    How it works
+                </p>
+                <p className="mb-8">Here’s how Freebyz works.</p>
 
-            
 
-            <div className='relative flex pt-6 pb-6 mt-2 overflow-x-hidden transition-all duration-300 md:hidden'
+                <div className="hidden gap-4 md:flex">
+                    {kickstartJourney.map((el, i) => (
+                        <div key={i} className="px-5 pt-5 md:w-96 bg-customGray">
+                            <p className='text-3xl font-medium leading-10 md:text-base'>{el.title}</p>
+                            <p className="h-40 mt-8 overflow-hidden md:h-60 md:text-base">{el.description}</p>
+
+                            <img src={el.image} alt="kickstart-journey-image" />
+
+                        </div>
+                    ))}
+                </div>
+
+                <div className='relative flex pt-6 pb-6 mt-2 overflow-x-hidden transition-all duration-300 md:hidden'
                 >
                     <Swiper
                         slidesPerView={1}
@@ -42,27 +43,21 @@ const KickstartJourney = () => {
                         className="process-swiper"
                     >
 
-                    {kickstartJourney.map((el, i) => (
-                        <SwiperSlide key={i} >
-                            <div className="w-full p-5 bg-customGray h-120">
-                                <p className='text-3xl font-medium leading-10 md:text-base'>{el.title}</p>
-                                <p className="md:text-base">{el.description}</p>
-                                <img src={el.image} alt="kickstart-journey-image" />
-                            </div>
-                        </SwiperSlide>
-                    ))} 
+                        {kickstartJourney.map((el, i) => (
+                            <SwiperSlide key={i} >
+                                <div className="w-full p-5 bg-customGray h-120">
+                                    <p className='text-3xl font-medium leading-10 md:text-base'>{el.title}</p>
+                                    <p className="md:text-base">{el.description}</p>
+                                    <img src={el.image} alt="kickstart-journey-image" />
+                                </div>
+                            </SwiperSlide>
+                        ))}
                     </Swiper>
                 </div>
-
-                
-                
-            
-            
-                    <Link><button to="/registration" className="px-4 py-3 my-8 text-white rounded-full bg-customBlue">Sign up for free</button></Link>
-            
+                <Link><button to="/registration" className="px-4 py-3 my-8 text-white rounded-full bg-customBlue">Sign up for free</button></Link>
+            </div>
         </div>
 
-        
     )
 };
 
