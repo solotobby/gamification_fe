ef03eb97a0f56721f634ac14ca2284dac1fea048
Whoistolu <digittolu25@gmail.com>
2025-02-12 16:10:40 +0100

    Create Affiliate component

---
+++ src/components/landing-page/AffiliateProgram.jsx
@@ -0,0 +1,56 @@
+
+
+const AffiliateProgram = () => {
+    return (
+        <div className="container pt-10 md:pt-20">
+           <div className="bg-[#F0F8FF]">
+           <div className="mt-12">
+                <h1 className="p-20 text-5xl font-medium leading-10 text-center text-white md:text-5xl bg-customBlue">Freebyz Affiliate Program</h1>
+            </div>
+
+            <div className="my-20 mb-8 ">
+                <h2 className="text-5xl font-medium ">Freebyz Affiliate program</h2>
+                <p className="my-4">Freebyz Affiliate program offers rewards to users as affiliate commissions for referring friends to verify their account and use Freebyz to access remote Jobs or grow their social media or business. We currently have two types of referral programs on Freebyz</p>
+            </div>
+
+            <div className="text-base leading-relaxed ">
+                <p className="mb-8 ">
+                    1. Direct referral program: When you refer a verified user, we pay you for every referral. Using this simple referral principle, users have made millions by making review videos and social media posts using their referral links.
+                    <p>Apart from referral commission, We offer amazing special referral bonuses in 3 levels:</p>
+                    <p className="mb-8">
+                        i.  Refer 50+ verified users in a month to earn N25k + extra N5000 from Freebyz.<br/>
+                        ii. Refer 20 verified users in a month to earn N10k + extra N1500<br/>
+                        iii. Refer 10 verified users in a month to earn N5k + extra N500 from Freebyz
+                    </p>
+                    <p className="mb-8"> 2. Third Party referral program: Freebyz has partnered with Wellahealth, an healthcare provider to give you access to referral bonuses when you refer your friends to sign up for an health care (insurance) plan offered by Wellahealth. With this, you can earn up to 200,000 Naira daily by referring your friends to Wellahealth. Wellahealth provide access to affordable health care for access to free drugs, blood tests, medical check-ups, free calls to Doctors, etc. You can gift the health care plan as a birthday gift. Health care will be obtained in the nearest Pharmacy or Hospital closest to the beneficiary.</p>
+                </p>
+
+                <p className="my-4 italic">
+                    <span className="font-semibold "> Remote Job opportunities in Nigeria — use your mobile phone to work and earn legit money daily in Nigeria —</span>
+                    The world has indeed witnessed a significant transformation in job opportunities, and Nigeria is no exception. The advent of technology and the COVID-19 pandemic has accelerated the demand for remote jobs. Freebyz is one of the legitimate sites making it easier for individuals to find legitimate remote work and grow their online presence.
+                </p>
+                <p className="my-4 italic">
+                    <span className="font-semibold "> How did remote Jobs start in Nigeria/Africa?</span>
+                    The COVID-19 pandemic may have passed, but its impact on the job market remains. With the sudden shift to remote work, many Nigerians discovered the benefits of working from home, including increased productivity, reduced commuting time, and improved work-life balance. As a result, the demand for remote jobs has continued to grow, with more businesses adopting flexible work arrangements to attract and retain top talent.
+                </p>
+
+                <p className="font-semibold">Remote jobs offer a range of benefits for you, including:</p>
+                <p><span className="font-semibold">- Flexibility:</span> You can Work from anywhere, at any time, once you’re connected online.</p>
+                <p><span className="font-semibold">- Increased productivity: </span> Reduce distractions and optimize your work for more productivity. With Freebyz, you don’t need a skill to work, submit evidence and earn.</p>
+                <p><span className="font-semibold">- Improved work-life balance:</span> You can spend more time with family and friends.</p>
+                <p><span className="font-semibold">- Reduced commuting time: </span> Save time and money by avoiding daily commutes.</p>
+
+                <p className="mt-8 "><span className="font-semibold">FREEBYZ</span> is a remote job and affiliate marketing website that connects workers with businesses and creators. With Freebyz, users can earn money by completing tasks online, promoting businesses and social media posts, and growing their online presence. Freebyz allows you to earn in dollars and convert your earnings to naira. You can also Find legitimate remote jobs and gigs.</p>
+                <p>- Promote businesses and social media posts to earn affiliate commissions ( for your clients) and grow your online presence.
+                    - Get paid weekly for your work and referrals. You can withdraw every friday.<br/>
+                    - Freebyz rewards users for referring users to their platform. You can $1.5 dollars on each referral and millions of naira by referring friends to Freebyz.<br/>
+                    - You can grow your social media presence on YouTube, TikTok, Facebook, Instagram, by getting unique and organic traffic followers, subscribers views likes and comments for your posts.<br/>
+                    - You can Monitor workers and approve or deny tasks to ensure quality and organic growth.
+                </p>
+            </div>
+           </div>
+        </div>
+    )
+}
+
+export default AffiliateProgram;
