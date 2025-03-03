a27c8592b4801f09fa3cbfedf41e1160c0560e97
Whoistolu <digittolu25@gmail.com>
2025-03-03 11:09:39 +0100

    Update URL

--- src/Services/jobs.jsx
+++ src/Services/jobs.jsx
@@ -2,7 +2,7 @@ import api from "./api";
 
 export const fetchJob = async () => {
     try {
-        const res = await api.get(`/jobs/available-jobs?page=1&subcategory_id=2`);
+        const res = await api.get(`/jobs/available-jobs`);
         return res.data?.data || [];
     } catch (error) {
         console.error("Error fetching jobs:", error);
